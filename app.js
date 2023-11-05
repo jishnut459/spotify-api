59088927c97a3bb84cb497397f3c92131086dce8
jishnu <jishnut459@gmail.com>
2023-11-05 18:18:40 +0530

    Add redirect uri to env

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'http://localhost:3000/callback';
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 
 
