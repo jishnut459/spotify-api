a83fd9d0165b00dbf192b365b77cfa646ed71e92
jishnu <jishnut459@gmail.com>
2023-11-06 15:43:29 +0530

    Modify code

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
-const uuid = require('uuid');
 const cors = require('cors');
-
 
 const app = express();
 require('dotenv').config();
@@ -13,140 +11,116 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-
-
 app.use(cookieParser());
 app.use(cors({
     origin: ['http://localhost:4200'], // Replace with your Angular app's domain
     credentials: true, // Enable cookies and credentials
 }));
 
-  
-
-const accessTokens = {};
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.get('/login', (req, res) => {
+// Helper function to generate a random string for state
+function generateRandomString(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+// Spotify login route with async/await and error handling
+app.get('/login', async (req, res) => {
     try {
-        const scope = 'user-top-read';
-        const authorizationUrl = `https://accounts.spotify.com/authorize?${querystring.stringify({
+        const state = generateRandomString(16);
+        const scope = 'user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private';
+
+        const authorizeUrl = 'https://accounts.spotify.com/authorize?' + querystring.stringify({
             response_type: 'code',
             client_id: CLIENT_ID,
-            scope,
+            scope: scope,
             redirect_uri: REDIRECT_URI,
-        })}`;
-
-        res.redirect(authorizationUrl);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error occurred while initiating Spotify authentication.');
-    }
-});
-
-
-app.get('/callback', async (req, res) => {
-    const code = req.query.code;
-
-    try {
-        const response = await axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
-            code,
-            redirect_uri: REDIRECT_URI,
-            grant_type: 'authorization_code',
-        }), {
-            headers: {
-                'Authorization': `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
+            state: state,
         });
 
-        // Generate a unique session ID using uuid
-        const sessionId = uuid.v4();
-
-        // Store access token in the accessTokens object with the session ID
-        const accessToken = response.data.access_token;
-        accessTokens[sessionId] = accessToken;
-
-        // Set the session ID as a secure cookie
-        res.cookie('sessionId', sessionId, { httpOnly: true, secure: true, domain: 'localhost'});
-
-        // Create a playlist after receiving the access token
-        await createPlaylist(accessToken);
-        // Redirect the user to the Angular app
-        // res.redirect('/top-songs');
+        res.redirect(authorizeUrl);
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error occurred while authenticating with Spotify.');
-        
+        console.error('Error in /login:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 });
-app.get('/get-access-token', (req, res) => {
-    const sessionId = req.cookies.sessionId;
 
-    // Check if the session ID exists in the accessTokens object
-    if (accessTokens[sessionId]) {
-        res.json({ access_token: accessTokens[sessionId] });
+
+// Callback route to handle the response from Spotify
+app.get('/callback', async function (req, res) {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+
+    if (state === null) {
+        res.redirect('/login' +
+            querystring.stringify({
+                error: 'state_mismatch',
+            }));
     } else {
-        res.status(404).json({ error: 'Access token not found for this session' });
-    }
-});
-
-app.get('/top-songs', async (req, res) => {
-    const sessionId = req.cookies.sessionId; // Retrieve the session ID from the secure cookie
-
-    // Check if the session ID exists in the accessTokens object
-    if (accessTokens[sessionId]) {
-        const accessToken = accessTokens[sessionId];
-
         try {
-            const response = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+            const authOptions = {
+                url: 'https://accounts.spotify.com/api/token',
+                form: {
+                    code: code,
+                    redirect_uri: REDIRECT_URI,
+                    grant_type: 'authorization_code',
+                },
                 headers: {
-                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
                 },
-                params: {
-                    limit: 10, // You can change the limit to fetch more top songs
-                },
+                json: true,
+            };
+
+            // Exchange the code for an access token
+            const response = await axios.post(authOptions.url, querystring.stringify(authOptions.form), {
+                headers: authOptions.headers,
             });
 
-            const topSongs = response.data.items;
-
-            // Create a new playlist with the user's top songs
-            const playlistResponse = await axios.post('https://api.spotify.com/v1/me/playlists', {
-                name: 'My Top Songs', // Set the playlist name
-                public: false, // Set the playlist as private
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`,
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            const playlistId = playlistResponse.data.id;
-
-            // Add the top songs to the newly created playlist
-            const uris = topSongs.map((song) => song.uri);
-            await axios.post(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-                uris,
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`,
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            // Send the list of top songs as the response
-            res.json({ topSongs, playlistId });
+            // Store the access token in a cookie for later use
+            res.cookie('access_token', response.data.access_token, { httpOnly: true });
 
         } catch (error) {
-            console.error(error);
-            res.status(500).send('Error creating and adding top songs to the playlist.');
+            res.status(500).json({ error: 'Error exchanging code for access token' });
         }
-    } else {
-        // If the session ID is not found, redirect the user to the authentication route
-        res.redirect('/login'); // Replace with the actual authentication route
     }
+});
+
+// Define the User Details API endpoint
+app.get('/api/user', function (req, res) {
+    // Retrieve the access token from the cookie
+    const access_token = req.cookies.access_token;
+
+    if (!access_token) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+    }
+
+    // Make an authorized request to Spotify's API to get user details
+    axios.get('https://api.spotify.com/v1/me', {
+        headers: {
+            'Authorization': `Bearer ${access_token}`,
+        },
+    })
+    .then(response => {
+        const user = {
+            display_name: response.data.display_name,
+            email: response.data.email,
+            // Add more user details here as needed
+        };
+        res.status(200).json(user);
+    })
+    .catch(error => {
+        console.error('Error fetching user details:', error);
+        res.status(500).json({ error: 'Error fetching user details' });
+    });
 });
 
 
